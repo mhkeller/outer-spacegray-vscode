19f9dd69bf55e9712262263c0c87fda45f0c84e7
mhkeller <code@mhkeller.com>
2021-01-07 17:55:34 -0500

    update styles

---
+++ example.js
@@ -0,0 +1,39 @@
+var myModule = require('my-module')
+import * as myModuleTwo from 'my-module'
+import { myModuleThree } from 'my-other-module'
+
+function Person (id) {
+  var self = this
+  const { scope: [admin] } = myModuleThree;
+  self.id = id
+  self.scope = admin
+}
+
+Person.prototype.initialize = function () {
+  var names = ['hello', 'goodbye', hi, yo]
+  var index = Math.round(Math.random())
+  this.name = names[index]
+}
+
+Person.prototype.greet = function ({ message = 'hello', gone }) {
+  var options = {
+    coming: message || 'hello',
+    going: 'goodbye',
+    gone
+  }
+  var index = Math.round(Math.random())
+  var key = Object.keys(options)[index]
+  return options[key] + sum
+}
+
+var person1 = new Person(1)
+console.log(person1.greet().coming)
+
+module.exports = {Person, myModule: hey}
+
+// es6 module
+export default myModuleTwo
+
+export function otherExport () {
+  return 'other';
+}
